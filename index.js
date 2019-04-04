8ad2a21dbfa5061e917c8c6a1405755dc191ea05
Dom Barter <dominicbarter@gmail.com>
2019-04-04 14:27:22 +0100

    can now automate posting of data

--- index.js
+++ index.js
@@ -1,71 +1,117 @@
-// var readline = require("readline"); //imports
-// var request = require("request");
-// var roundTo = require('round-to');
+var readline = require("readline"); //imports
+var request = require("request");
+var roundTo = require('round-to');
 
-// function writeWaitingPercent(p) { //updates the console
-//     readline.clearLine(process.stdout);
-//     readline.cursorTo(process.stdout, 0);
-//     process.stdout.write(`${p}% Complete`);
-// }
-
-// var counter = 0; //current iteration
-// var total = 150; //total number to register
-// var successful = 0; //total number of successful hits
-
-// function registerUser(){ //register user function
-//     var userToRegister = {
-//         apiPassword:"api-pass-123",
-// 	    name:`User ${counter}`,
-// 	    email:`${counter}@insight.com`,
-// 	    password:"password123",
-// 	    code:"insight-walking-app"
-//     }
-
-//     return new Promise(function(resolve,reject){
-//         request({ // post the data to the api
-//             url: "https://i7lis7jv3f.execute-api.eu-west-2.amazonaws.com/prod/users/register",
-//             method: "POST",
-//             json: true, 
-//             body: userToRegister
-//         }, 
-//         function (error, response, body){
-//             if(body.success === false){ //if there was an error redirect to index
-//                 reject(false);
-//             }
-//             else{
-//                 resolve(true);
-//             }
-//         });
-//     })
-    
-// }
-
-// function main(){ //main function
-
-//     writeWaitingPercent(0);
-//     var interval = setInterval(async function(){
-//         if(counter === total){
-//             clearInterval(interval);
-//             console.log("\n\nFinished!")
-//             console.log(`${(successful/total)*100}% Successful`); //final print
-//         }
-//         else{
-//             await registerUser().then(function(success){
-//                 successful = successful + 1;
-//                 counter = counter + 1;
-//                 writeWaitingPercent(roundTo((counter/total)*100,2));
-//             }).catch(function(failed){
-//                 counter = counter + 1;
-//                 writeWaitingPercent(roundTo((counter/total)*100,2));
-//             });
-//         }
-//     },7500)  
-
-    
-// }
-
-// main();
+function writeWaitingPercent(p) { //updates the console
+    readline.clearLine(process.stdout);
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${p}% Complete`);
+}
 
 exports.helloWorld = function(){
     console.log("Hello World!");
 }
+
+// Main Start Function ------------------------------------
+
+exports.start = function(options){
+
+    // verify that all components are present in options
+
+    var missingComponents = [];
+    if(options.number === undefined){missingComponents.push("number")}
+    if(options.delay === undefined){missingComponents.push("delay")}
+    if(options.endpoint === undefined){missingComponents.push("endpoint")}
+    if(options.data === undefined){missingComponents.push("data")}
+
+    if(missingComponents.length > 0){
+        console.error("You have missing elements from options: ");
+        console.error(JSON.stringify(missingComponents));
+        return false;
+    }
+
+    // default certain items to their standard values
+
+    if(options.delay < 5){options.delay = 5; console.log("Delay was set to default value of 5 seconds")};
+    if(options.number < 1){options.number = 1; console.log("Number was set to defualt value of 1")};
+
+    // find the prperties with the wildcard property
+
+    var allProperties = Object.getOwnPropertyNames(options.data);
+    var wildcardProperties = [];
+    for(var i = 0; i < allProperties.length; i++){
+        if(isNaN(options.data[allProperties[i]])){
+            if(options.data[allProperties[i]].search("<% numerical %>") !== -1){
+                wildcardProperties.push(allProperties[i]);
+            }
+        }
+    }
+
+    // set up the basic post function
+
+    function postData(dataToPost_){
+        return new Promise(function(resolve,reject){
+            request({
+                url:options.endpoint,
+                method:"POST",
+                json: true,
+                body: dataToPost_,
+            },
+            function(error,response,body){
+                resolve(true)
+            })
+        })
+    }
+
+    // set up the intial progress bar
+
+    writeWaitingPercent(0);
+
+    // set up the base variables
+
+    var counter = 0; //current iteration
+    var total = options.number; //total number to post
+    var dataToPost;
+
+    // begin posting the data
+
+    var interval = setInterval(async function(){
+        if(counter === total){
+            clearInterval(interval)
+            console.log("\n\nFinished!");
+        }
+        else{
+            // create the data to post
+            dataToPost = {}; //makes empty object
+
+            for(var j = 0; j < allProperties.length; j++){ //creates new obejct properties
+                dataToPost[allProperties[j]] = options.data[allProperties[j]];
+            }
+
+            if(wildcardProperties.length > 0){ //assigns all wildcards
+                for(var i =0; i < wildcardProperties.length; i++){
+                    dataToPost[wildcardProperties[i]] = (dataToPost[wildcardProperties[i]]).replace("<% numerical %>",`${counter}`);
+                }
+            }
+            console.log(dataToPost);
+
+            await postData(dataToPost)
+            counter = counter + 1;
+            writeWaitingPercent(roundTo((counter/total)*100,2));
+        }
+    }, (options.delay * 1000))
+}
+
+// var options_ = {
+//     number: 5,
+//     delay: 5,
+//     endpoint: "https://i7lis7jv3f.execute-api.eu-west-2.amazonaws.com/prod/teams/create",
+//     data:{
+//         apiPassword:"api-pass-123",
+// 	    teamName:"Team <% numerical %>",
+// 	    email:"<% numerical %>@insight.com",
+// 	    location:"London, GB"
+//     }
+// }
+
+// exports.start(options_);
